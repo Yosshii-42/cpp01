3d786a714d035f7ff9df900eceac335f1b433a84
Yoshika Tsurudome <161409687+Yosshii-42@users.noreply.github.com>
2025-02-13 17:15:44 +0900

    Add files via upload

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,27 +1,29 @@
 #include "Harl.hpp"
 
 void    Harl::_debug() {
-    std::cout << "I love having extra bacon for my "
-              << "7XL-double-cheese-triple-pickle-special-"
-              << "ketchup burger. I really do!"
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger."
+              << std::endl
+              << "I really do!"
               << std::endl;
 }
 void    Harl::_info() {
-    std::cout << "I cannot believe adding extra bacon "
-              << "costs more money. You didn’t put "
-              << "enough bacon in my burger! If you did,"
-              << " I wouldn’t be asking for more!"
+    std::cout << "I cannot believe adding extra bacon costs more money."
+              << std::endl
+              << "You didn't put enough bacon in my burger!"
+              << std::endl
+              << "If you did, I wouldn't be asking for more!"
               << std::endl;
 }
 void    Harl::_warning() {
-    std::cout << "I think I deserve to have some extra bacon"
-              << " for free. I’ve been coming foryears whereas"
-              << " you started working here since last month."
+    std::cout << "I think I deserve to have some extra bacon for free."
+              << std::endl
+              << "I've been coming foryears whereas you started working here since last month."
               << std::endl;
 } 
 void    Harl::_error() {
-    std::cout << "This is unacceptable! I want to speak to "
-              << "he manager now."
+    std::cout << "This is unacceptable!"
+              << std::endl
+              << "I want to speak to the manager now."
               << std::endl;
 }
 void    Harl::_argError() {
